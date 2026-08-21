8ddd7bcdd35c30629c9addff5e9028c869dd9100
CyberStill-GmbH <cesar.guevara.s@uni.pe>
2026-08-21 15:10:56 -0500

    refactor(normalizer): separa componente del collector

---
+++ normalizer/__init__.py
@@ -0,0 +1,5 @@
+"""Validation and normalization of raw HoneyTrace events."""
+
+from normalizer.normalizer import EventValidationError, normalize_event
+
+__all__ = ["EventValidationError", "normalize_event"]
